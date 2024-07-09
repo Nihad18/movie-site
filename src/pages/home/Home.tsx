f57cf3e3f3e1835d082e972d53f630604707fd63
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-09 15:20:05 +0400

    added CarouselContainer component to Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,15 +1,14 @@
+import { CarouselContainer } from "@/components/ui/CarouselContainer";
 
 const Home = () => {
-  return (
-    <div className="text-white dark:bg-black pt-[88px]">
-      <div className="container dark:text-white text-black">
-        <h1 >
-          carousel adding here
-        </h1>
-        <h1>Home</h1>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className='text-white dark:bg-black pt-[88px]'>
+            <div className='container text-black dark:text-white'>
+                <CarouselContainer />
+                <h1>Home</h1>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
