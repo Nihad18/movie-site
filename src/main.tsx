f7e8c1bfe3706b840250d7fab885918a36515d9f
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-09 15:18:41 +0400

    feat: added QueryClientProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import ReactDOM from 'react-dom/client'
-import './styles/main.scss'
-import { RouterProvider } from 'react-router-dom'
-import { routes } from './routes'
-import { DataProvider } from './context/MainContext'
+import ReactDOM from "react-dom/client";
+import "./styles/main.scss";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
+import { DataProvider } from "./context/MainContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <DataProvider>
-    <RouterProvider router={routes} />
-  </DataProvider>,
-)
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
+        <DataProvider>
+            <RouterProvider router={routes} />
+        </DataProvider>
+        ,
+    </QueryClientProvider>,
+);
