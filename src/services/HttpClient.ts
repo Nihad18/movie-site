89d0cffda5a39321a0b8c7488d791989ab192108
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-09 15:09:44 +0400

    feat: added HttpClient for handle api requests

---
+++ src/services/HttpClient.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosResponse } from 'axios';
+
+export class HttpClient {
+    static apiToken = import.meta.env.VITE_TMDB_API_TOKEN;
+    static baseURL = import.meta.env.VITE_TMDB_BASE_URL;
+
+    static async get<T>(path: string): Promise<AxiosResponse<T>> {
+        return axios.get<T>(`${this.baseURL}${path}`, {
+            headers: {
+                'Authorization': `Bearer ${this.apiToken}`
+            }
+        });
+    }
+}
+
