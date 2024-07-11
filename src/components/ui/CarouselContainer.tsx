1f9ed5e231ee25027c720d242952f8609ec8adb1
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-11 11:09:16 +0400

    fix: carousel load img

--- src/components/ui/CarouselContainer.tsx
+++ src/components/ui/CarouselContainer.tsx
@@ -8,17 +8,22 @@ import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/Carouse
 
 import MovieService from "@/services/MovieService";
 
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { Button } from "./Button";
+
 export function CarouselContainer() {
     const { activeCarouselIndex } = useContext(DataContext);
     const { data, isLoading } = useQuery({ queryKey: ["fetchData"], queryFn: () => MovieService.getAll() });
     if (isLoading) console.log("loading...");
     return (
         <div className='relative max-w-sm min-w-full select-none carousel-image'>
-            <img
-                className='absolute top-0 left-0 object-cover h-[100vh] w-[100vw] '
-                src={`https://image.tmdb.org/t/p/original/${data?.data?.results?.[activeCarouselIndex]?.backdrop_path}`}
+            <LazyLoadImage
                 alt={`${data?.data?.results?.[activeCarouselIndex]?.title} Poster`}
-                loading='lazy'
+                effect='blur'
+                src={`https://image.tmdb.org/t/p/original/${data?.data?.results?.[activeCarouselIndex]?.backdrop_path}`}
+                placeholderSrc='placeholder.jpg'
+                wrapperClassName='carousel-poster-image'
             />
             <Carousel
                 opts={{
@@ -26,34 +31,33 @@ export function CarouselContainer() {
                     dragFree: true,
                     loop: true,
                 }}
-                className='w-full relative z-10'
+                className='relative z-10 w-full'
             >
                 <CarouselContent>
                     {data?.data?.results?.map((item: any, index: any) => (
                         <CarouselItem key={index} id={`${index}`}>
                             <Card
-                                className={`mx-[23px] cursor-pointer relative transition-[width,height] ${index == activeCarouselIndex
-                                    ? "w-[287px!important] h-[383px!important]"
-                                    : "w-[124px!important] h-[179px!important]"
-                                    }`}
+                                className={`mx-[23px] cursor-pointer relative transition-[width,height] duration-300 overflow-hidden ${
+                                    index == activeCarouselIndex ? "w-[287px] h-[383px]" : "w-[124px] h-[179px]"
+                                }`}
                             >
                                 <CardContent className='flex flex-col items-center justify-end w-full h-full aspect-square'>
                                     {index == activeCarouselIndex && (
-                                        <div className='mb-[27px] text-center'>
+                                        <div className='mb-[27px] text-center animate-slideIn delay-[400ms] z-[2]'>
                                             <div className='text-[20px] text-white text-center mb-[13px]'>{item.title}</div>
                                             <div>
-                                                <button className='w-[123px] h-[36px] px-[25px] py-[8px] rounded-[5px] text-white bg-[#AB0A10]'>
+                                                <Button className=' h-[36px] w-[123px] px-[25px] py-[8px] rounded-[5px] text-white bg-primary'>
                                                     Book now
-                                                </button>
+                                                </Button>
                                             </div>
                                         </div>
                                     )}
-                                    <img
-                                        className={`object-cover z-[-1] top-0 left-0 absolute w-full h-full rounded-[5px] ${index != activeCarouselIndex ? "img-overlay" : ""
-                                            }`}
-                                        src={`https://image.tmdb.org/t/p/original/${item?.poster_path}`}
-                                        alt={`${item.title} Poster`}
-                                        loading='lazy'
+                                    <LazyLoadImage
+                                        alt={`${item?.title} Poster`}
+                                        effect='blur'
+                                        src={`https://image.tmdb.org/t/p/original/${item?.backdrop_path}`}
+                                        placeholderSrc='placeholder.jpg'
+                                        wrapperClassName={`carousel-card-image`}
                                     />
                                 </CardContent>
                             </Card>
