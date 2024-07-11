f84e841bf146d4c996d2fec804c74dc02449a648
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-11 12:05:22 +0400

    feat:added light and dark theme classes

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -7,41 +7,60 @@ const Timeline = () => {
     const minTime = Math.min(...histories);
     const maxTime = Math.max(...histories);
     const calculateLeftPercentage = (time: number) => {
-        return ((time - minTime) / (maxTime - minTime)) * (3 / 4) * 100;
+        return ((time - minTime) / (maxTime - minTime)) * 75;
     };
     const handleMarkerClick = (time: number) => {
         setSelectedTime(time);
     };
     return (
-        <div className='w-[1312px] mx-auto'>
-            <div className='text-white text-[25px] font-medium mb-[23px]'>Pearls of Azerbaijani cinema</div>
-            <div className='w-[1140px] mx-auto pt-[27px]'>
-                <div className={`h-[42px] flex items-center mb-[81px] ${styles.lineContainer}`}>
-                    <div className={styles.line}></div>
-                    <div className={styles.fillLine} style={{ width: `${calculateLeftPercentage(selectedTime + 10)}%` }}></div>
-                    {histories.map((time) => (
+        <div className='dark:bg-black-alt pt-[110px] pb-[82px]'>
+            <div className='container'>
+                <div className='text-gray-darker-alt2 dark:text-white text-[25px] font-medium pb-[23px]'>Pearls of Azerbaijani cinema</div>
+                <div className='w-[1140px] mx-auto pt-[27px] pb-[28px]'>
+                    <div className={`h-[42px] flex items-center mb-[81px] ${styles.lineContainer}`}>
+                        <div className={`bg-gray-lightest-alt5 dark:bg-gray-darkest-alt ${styles.line}`}></div>
                         <div
-                            key={time}
-                            className={`${styles.markerContainer}`}
-                            style={{ left: `${calculateLeftPercentage(time + 10)}%` }}
-                            onClick={() => handleMarkerClick(time)}
-                        >
-                            <div className={`${time == selectedTime ? styles.activeMarkerLabel : styles.markerLabel}`}>{time}</div>
-                            <div className={`${time == selectedTime ? styles.activeMarker : styles.marker}`}></div>
-                        </div>
-                    ))}
-                </div>
-                <div className='w-[1084px] flex justify-between items-center'>
-                    <div className='w-[558px] h-[432px]'>
-                        <img className='w-full h-full' src={MoviePoster} />
+                            className={`bg-black dark:bg-white ${styles.fillLine}`}
+                            style={{ width: `${calculateLeftPercentage(selectedTime + 10)}%` }}
+                        ></div>
+                        {histories.map((time) => (
+                            <div
+                                key={time}
+                                className={`${styles.markerContainer}`}
+                                style={{ left: `${calculateLeftPercentage(time + 10)}%` }}
+                                onClick={() => handleMarkerClick(time)}
+                            >
+                                <div
+                                    className={`${
+                                        time == selectedTime
+                                            ? `text-black dark:text-white ${styles.activeMarkerLabel}`
+                                            : `text-gray-lightest-alt5 dark:text-gray-normal-alt ${styles.markerLabel}`
+                                    }`}
+                                >
+                                    {time}
+                                </div>
+                                <div
+                                    className={`${
+                                        time == selectedTime
+                                            ? `bg-black dark:bg-white  ${styles.activeMarker}`
+                                            : `bg-gray-lightest-alt5 dark:bg-gray-normal-alt"  ${styles.marker}`
+                                    }`}
+                                ></div>
+                            </div>
+                        ))}
                     </div>
-                    <div className='w-[415px] text-white'>
-                        <div className='font-light text-[11px] pb-[6px]'>OPERETTA</div>
-                        <div className='font-medium text-[25px] pb-[30px]'>Arshin Mal Alan</div>
-                        <div className='font-medium text-[14px]'>
-                            Arshin Mal Alan is a 4-act operetta composed by Azerbaijani composer Uzeyir Hajibeyov. The libretto of the work was also
-                            written by Uzeyir Hajibeyov in 1913 in St. Petersburg. The premiere of the opera was held on October 25, 1913 at the Haji
-                            Zeynelabidin Tagiyev Theater in Baku.
+                    <div className='w-[1084px] flex justify-between items-center'>
+                        <div className='w-[558px] h-[432px]'>
+                            <img className='w-full h-full' src={MoviePoster} />
+                        </div>
+                        <div className='w-[415px]'>
+                            <div className='font-light text-[11px] pb-[6px] text-black dark:text-white'>OPERETTA</div>
+                            <div className='font-medium text-[25px] pb-[30px] text-gray-darker-alt2 dark:text-white'>Arshin Mal Alan</div>
+                            <div className='font-medium text-[14px] text-gray-normal-alt2 dark:text-gray-lighter-alt5'>
+                                Arshin Mal Alan is a 4-act operetta composed by Azerbaijani composer Uzeyir Hajibeyov. The libretto of the work was
+                                also written by Uzeyir Hajibeyov in 1913 in St. Petersburg. The premiere of the opera was held on October 25, 1913 at
+                                the Haji Zeynelabidin Tagiyev Theater in Baku.
+                            </div>
                         </div>
                     </div>
                 </div>
