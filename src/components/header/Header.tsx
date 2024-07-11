b8443c2498432f3fbd05a69b8c8bafe25f9973f2
MuxammedL <layicovmuxammed@gmail.com>
2024-07-11 10:30:58 +0400

    feat: if user logined then showing logout button otherwise account icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,9 +2,12 @@ import { useContext } from "react";
 import styles from "./header.module.scss";
 import { DataContext } from "@/context/MainContext";
 import { Link } from "react-router-dom";
+import { useAuth } from "@/context/AuthContext";
+import { Button } from "../ui/Button";
 
 const Header = () => {
   const { theme, setTheme } = useContext(DataContext)
+  const user = useAuth();
   return (
     <header className={`${styles.header}`}>
       <div className="container">
@@ -17,9 +20,12 @@ const Header = () => {
               <input type="checkbox" checked={theme == "dark"} onChange={() => setTheme(prev => prev == "dark" ? "light" : "dark")} />
               <span className={styles.slider}></span>
             </label>
-            <Link to="/login" className="profile block p-[6px] cursor-pointer">
-              <img src="/account.svg" alt="account-icon" />
-            </Link>
+            {
+              user?.token ? <Button onClick={user?.logOut}>Logout</Button> :
+                <Link to="/login" className="profile block p-[6px] cursor-pointer">
+                  <img src="/account.svg" alt="account-icon" />
+                </Link>
+            }
           </div>
         </div>
       </div>
