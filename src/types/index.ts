0c94d01d2a3fba00901343c641982e49a1ee78ae
MuxammedL <layicovmuxammed@gmail.com>
2024-07-11 10:06:12 +0400

    feat: added types

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type LoginData = {
+    email: string;
+    password: string;
+};
+
+export type AuthContextType = {
+    token: string;
+    user: any;
+    loginAction: (data: LoginData) => Promise<void>;
+    logOut: () => void;
+};
