15e0f5499b12a23d7885b5cebbf6b88342f4b359
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-12 19:53:57 +0400

    feat: added LatestNews component to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import Timeline from "@/components/timeline/Timeline";
 import { CarouselContainer } from "@/components/ui/CarouselContainer";
+import LatestNews from "@/components/ui/LatestNews";
 
 const Home = () => {
     return (
         <section>
             <CarouselContainer />
             <Timeline />
+            <LatestNews/>
         </section>
     );
 };
