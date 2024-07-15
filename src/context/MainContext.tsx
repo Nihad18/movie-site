e6b6bb95880027a718ce918613e6a2101411ddbb
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-15 12:32:18 +0400

    added new states for seat selection and selected seats components

--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -1,11 +1,27 @@
 import React, { createContext, useState, ReactNode } from "react";
 
+interface SelectedSeats {
+    rowId: number;
+    seatId: number;
+    row: string;
+    seatNumber: number;
+}
+
 interface DataContextProps {
     theme: string;
     setTheme: React.Dispatch<React.SetStateAction<"dark" | "light">>;
 
     activeCarouselIndex: number;
     setActiveCarouselIndex: React.Dispatch<React.SetStateAction<number>>;
+
+    activeDate: number;
+    setActiveDate: React.Dispatch<React.SetStateAction<number>>;
+
+    activeTime: string;
+    setActiveTime: React.Dispatch<React.SetStateAction<string>>;
+
+    selectedSeats: SelectedSeats[];
+    setSelectedSeats: React.Dispatch<React.SetStateAction<SelectedSeats[]>>;
 }
 
 interface DataProviderProps {
@@ -18,10 +34,39 @@ export const DataContext = createContext<DataContextProps>({
 
     activeCarouselIndex: 1,
     setActiveCarouselIndex: () => {},
+
+    activeDate: 0,
+    setActiveDate: () => {},
+
+    activeTime: "",
+    setActiveTime: () => {},
+
+    selectedSeats: [],
+    setSelectedSeats: () => {},
 });
 
 export const DataProvider: React.FC<DataProviderProps> = ({ children }) => {
     const [theme, setTheme] = useState<"dark" | "light">((localStorage.getItem("theme") as "dark" | "light") || "light");
     const [activeCarouselIndex, setActiveCarouselIndex] = useState(1);
-    return <DataContext.Provider value={{ theme, setTheme, activeCarouselIndex, setActiveCarouselIndex }}>{children}</DataContext.Provider>;
+    const [activeDate, setActiveDate] = useState<number>(0);
+    const [activeTime, setActiveTime] = useState<string>("");
+    const [selectedSeats, setSelectedSeats] = useState<SelectedSeats[]>([]);
+    return (
+        <DataContext.Provider
+            value={{
+                theme,
+                setTheme,
+                activeCarouselIndex,
+                setActiveCarouselIndex,
+                activeDate,
+                setActiveDate,
+                activeTime,
+                setActiveTime,
+                selectedSeats,
+                setSelectedSeats,
+            }}
+        >
+            {children}
+        </DataContext.Provider>
+    );
 };
