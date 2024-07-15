8e8ddd07a7ed764f43378397893805a6759a59f9
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-15 12:29:51 +0400

    feat: added movie order page

--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -1,8 +1,13 @@
+import SeatSelection from "@/components/ui/order/SeatSelection";
+import SelectedSeats from "@/components/ui/order/SelectedSeats";
 
 const Order = () => {
-  return (
-    <div className="text-white">Order</div>
-  )
-}
+    return (
+        <div className='flex w-[100vw] h-[100vh] bg-[#121212]'>
+            <SelectedSeats />
+            <SeatSelection />
+        </div>
+    );
+};
 
-export default Order
+export default Order;
