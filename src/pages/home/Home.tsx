fc336b646b4a0d0b113adcd0700c7be669abd957
MuxammedL <layicovmuxammed@gmail.com>
2024-07-15 16:33:15 +0400

    feat: added seemore components also when user loggined thwn show her/his already waatched films

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,22 @@
+import AboutUs from "@/components/aboutUs/AboutUs";
 import Timeline from "@/components/timeline/Timeline";
 import { CarouselContainer } from "@/components/ui/CarouselContainer";
 import LatestNews from "@/components/ui/LatestNews";
+import SeeMore from "@/components/ui/SeeMore";
+import { useAuth } from "@/context/AuthContext";
+import { filmList, filmList2 } from "@/lib/utils";
 
 const Home = () => {
+    const user = useAuth();
     return (
         <section>
             <CarouselContainer />
+            <SeeMore name="Curently playing" link="/curently-playing" data={filmList} />
+            <SeeMore name="Coming soon" link="/coming-soon" data={filmList2} />
+            {user?.token && <SeeMore name="Already watched movies" link="/already-watched-movies" data={filmList} />}
             <Timeline />
-            <LatestNews/>
+            <AboutUs />
+            <LatestNews />
         </section>
     );
 };
