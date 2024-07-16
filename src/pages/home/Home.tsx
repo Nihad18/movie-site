23309de7ff50bb23f74c681282cb1f5bfb3398bc
MuxammedL <layicovmuxammed@gmail.com>
2024-07-16 11:47:58 +0400

    feat: getting Seemore components data from API

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,16 +4,19 @@ import { CarouselContainer } from "@/components/ui/CarouselContainer";
 import LatestNews from "@/components/ui/LatestNews";
 import SeeMore from "@/components/ui/SeeMore";
 import { useAuth } from "@/context/AuthContext";
-import { filmList, filmList2 } from "@/lib/utils";
+import MovieService from "@/services/MovieService";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
     const user = useAuth();
+    const upComingFilms = useQuery({ queryKey: ["fetchData"], queryFn: () => MovieService.getUpComing() });
+    const onTheAirFilms = useQuery({ queryKey: ["fetchData"], queryFn: () => MovieService.getOnTheAir() })
     return (
         <section>
             <CarouselContainer />
-            <SeeMore name="Curently playing" link="/curently-playing" data={filmList} />
-            <SeeMore name="Coming soon" link="/coming-soon" data={filmList2} />
-            {user?.token && <SeeMore name="Already watched movies" link="/already-watched-movies" data={filmList} />}
+            <SeeMore name="Curently playing" link="/curently-playing" data={upComingFilms?.data?.data} />
+            <SeeMore name="Coming soon" link="/coming-soon" data={onTheAirFilms.data?.data} />
+            {user?.token && <SeeMore name="Already watched movies" link="/already-watched-movies" data={onTheAirFilms.data?.data} />}
             <Timeline />
             <AboutUs />
             <LatestNews />
