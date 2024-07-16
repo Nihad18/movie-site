e6fd46a5c45f0770631a5a9828cea40d2f1e86fb
MuxammedL <layicovmuxammed@gmail.com>
2024-07-16 11:46:59 +0400

    feat: created new function and some datas

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,4 @@
 import { FooterLinksData } from "@/types/common";
-import { FilmListData } from "@/types/filmDataTypes";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -19,72 +18,22 @@ export function formatDate(inputDate: Date) {
     return `${day} ${month} ${year}`;
 }
 
+export function filmDate(inputDate: string | undefined) {
+    if (!inputDate) return "";
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const date = new Date(inputDate);
+    const day = date.getDate();
+    const monthIndex = date.getMonth();
+    const month = months[monthIndex];
+
+    return `${day} ${month}`;
+}
+
 export const currentYear = new Date().getFullYear();
+
 export const years = Array.from({ length: currentYear - 1970 + 1 }, (_, i) => currentYear - i);
 
 export const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-export const filmList: FilmListData[] = [
-    {
-        image: "https://image.tmdb.org/t/p/original//gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
-        name: "Kingdom of the Planet of the Apes",
-        date: "20 April",
-        ageLimit: "16+",
-        languages: [{ lang: "En" }, { lang: "Ru" }],
-    },
-    {
-        image: "https://image.tmdb.org/t/p/original//t6HIqrRAclMCA60NsSmeqe9RmNV.jpg",
-        name: "Avatar: The Way of Water",
-        date: "16 December",
-        ageLimit: "12+",
-        languages: [{ lang: "En" }, { lang: "Fr" }],
-    },
-    {
-        image: "https://cdnb.artstation.com/p/assets/images/images/046/231/993/large/nima-neemz-nakhshab-dominion-final.jpg?1644599731",
-        name: "Jurassic World Dominion",
-        date: "10 June",
-        ageLimit: "13+",
-        languages: [{ lang: "En" }, { lang: "Es" }],
-    },
-    {
-        image: "https://image.tmdb.org/t/p/original//1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
-        name: "Spider-Man: No Way Home",
-        date: "17 December",
-        ageLimit: "13+",
-        languages: [{ lang: "En" }],
-    },
-];
-
-export const filmList2: FilmListData[] = [
-    {
-        image: "https://i.ebayimg.com/images/g/zRAAAOSwvaJgJSg1/s-l1600.jpg",
-        name: "The Batman",
-        date: "4 March",
-        ageLimit: "15+",
-        languages: [{ lang: "En" }, { lang: "De" }],
-    },
-    {
-        image: "https://image.tmdb.org/t/p/original//9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
-        name: "Doctor Strange in the Multiverse of Madness",
-        date: "6 May",
-        ageLimit: "13+",
-        languages: [{ lang: "En" }, { lang: "It" }],
-    },
-    {
-        image: "https://image.tmdb.org/t/p/original//yZevl2vHQgmosfwUdVNzviIfaWS.jpg",
-        name: "The Flash",
-        date: "3 November",
-        ageLimit: "14+",
-        languages: [{ lang: "En" }, { lang: "Pt" }],
-    },
-    {
-        image: "https://image.tmdb.org/t/p/original//7lTnXOy0iNtBAdRP3TZvaKJ77F6.jpg",
-        name: "Aquaman and the Lost Kingdom",
-        date: "16 December",
-        ageLimit: "12+",
-        languages: [{ lang: "En" }],
-    },
-];
 
 export const footerLinks: FooterLinksData[] = [
     {
@@ -114,3 +63,124 @@ export const footerLinks: FooterLinksData[] = [
         ],
     },
 ];
+
+export const seatData = [
+    {
+        rowId: 1,
+        row: "G",
+        seats: [
+            { seatId: 1, seatNumber: 1, status: "available" },
+            { seatId: 2, seatNumber: 2, status: "booked" },
+            { seatId: 3, seatNumber: 3, status: "available" },
+            { seatId: 4, seatNumber: 4, status: "available" },
+            { seatId: 5, seatNumber: 5, status: "booked" },
+            { seatId: 6, seatNumber: 6, status: "available" },
+        ],
+    },
+    {
+        rowId: 2,
+        row: "F",
+        seats: [
+            { seatId: 7, seatNumber: 1, status: "booked" },
+            { seatId: 8, seatNumber: 2, status: "booked" },
+            { seatId: 9, seatNumber: 3, status: "available" },
+            { seatId: 10, seatNumber: 4, status: "available" },
+            { seatId: 11, seatNumber: 5, status: "booked" },
+            { seatId: 12, seatNumber: 6, status: "available" },
+            { seatId: 13, seatNumber: 7, status: "available" },
+            { seatId: 14, seatNumber: 8, status: "booked" },
+        ],
+    },
+    {
+        rowId: 3,
+        row: "E",
+        seats: [
+            { seatId: 15, seatNumber: 1, status: "booked" },
+            { seatId: 16, seatNumber: 2, status: "booked" },
+            { seatId: 17, seatNumber: 3, status: "booked" },
+            { seatId: 18, seatNumber: 4, status: "booked" },
+            { seatId: 19, seatNumber: 5, status: "booked" },
+            { seatId: 20, seatNumber: 6, status: "available" },
+            { seatId: 21, seatNumber: 7, status: "available" },
+            { seatId: 22, seatNumber: 8, status: "available" },
+            { seatId: 23, seatNumber: 9, status: "available" },
+            { seatId: 24, seatNumber: 10, status: "available" },
+        ],
+    },
+    {
+        rowId: 4,
+        row: "D",
+        seats: [
+            { seatId: 25, seatNumber: 1, status: "available" },
+            { seatId: 26, seatNumber: 2, status: "booked" },
+            { seatId: 27, seatNumber: 3, status: "available" },
+            { seatId: 28, seatNumber: 4, status: "booked" },
+            { seatId: 29, seatNumber: 5, status: "available" },
+            { seatId: 30, seatNumber: 6, status: "booked" },
+            { seatId: 31, seatNumber: 7, status: "available" },
+            { seatId: 32, seatNumber: 8, status: "booked" },
+            { seatId: 33, seatNumber: 9, status: "available" },
+            { seatId: 34, seatNumber: 10, status: "booked" },
+            { seatId: 35, seatNumber: 10, status: "booked" },
+            { seatId: 36, seatNumber: 10, status: "booked" },
+        ],
+    },
+    {
+        rowId: 5,
+        row: "C",
+        seats: [
+            { seatId: 37, seatNumber: 3, status: "available" },
+            { seatId: 38, seatNumber: 4, status: "booked" },
+            { seatId: 39, seatNumber: 5, status: "available" },
+            { seatId: 40, seatNumber: 6, status: "booked" },
+            { seatId: 41, seatNumber: 7, status: "available" },
+            { seatId: 42, seatNumber: 8, status: "booked" },
+            { seatId: 43, seatNumber: 9, status: "available" },
+            { seatId: 44, seatNumber: 10, status: "booked" },
+            { seatId: 45, seatNumber: 10, status: "booked" },
+            { seatId: 46, seatNumber: 10, status: "booked" },
+            { seatId: 47, seatNumber: 10, status: "booked" },
+            { seatId: 48, seatNumber: 10, status: "booked" },
+        ],
+    },
+    {
+        rowId: 6,
+        row: "B",
+        seats: [
+            { seatId: 49, seatNumber: 5, status: "available" },
+            { seatId: 50, seatNumber: 6, status: "booked" },
+            { seatId: 51, seatNumber: 7, status: "available" },
+            { seatId: 52, seatNumber: 8, status: "booked" },
+            { seatId: 53, seatNumber: 9, status: "available" },
+            { seatId: 54, seatNumber: 10, status: "booked" },
+            { seatId: 55, seatNumber: 10, status: "booked" },
+            { seatId: 56, seatNumber: 10, status: "booked" },
+            { seatId: 57, seatNumber: 10, status: "booked" },
+            { seatId: 58, seatNumber: 10, status: "booked" },
+            { seatId: 59, seatNumber: 10, status: "booked" },
+            { seatId: 60, seatNumber: 10, status: "booked" },
+        ],
+    },
+    {
+        rowId: 7,
+        row: "A",
+        seats: [
+            { seatId: 61, seatNumber: 7, status: "available" },
+            { seatId: 62, seatNumber: 8, status: "booked" },
+            { seatId: 63, seatNumber: 9, status: "available" },
+            { seatId: 64, seatNumber: 10, status: "booked" },
+            { seatId: 65, seatNumber: 10, status: "booked" },
+            { seatId: 66, seatNumber: 10, status: "booked" },
+            { seatId: 67, seatNumber: 10, status: "booked" },
+            { seatId: 68, seatNumber: 10, status: "booked" },
+            { seatId: 69, seatNumber: 10, status: "booked" },
+            { seatId: 70, seatNumber: 10, status: "booked" },
+            { seatId: 71, seatNumber: 10, status: "booked" },
+            { seatId: 72, seatNumber: 10, status: "booked" },
+        ],
+    },
+];
+
+export const dates = [21, 22, 23, 24, 25, 26, 27];
+
+export const times = ["13:15", "15:15", "18:15", "20:30", "22:30"];
