39403b4de6d8684fd1af5649ce2306bfa7f3ad5b
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-17 12:07:44 +0400

    feat: added moive name to order page

--- src/pages/order/Order.tsx
+++ src/pages/order/Order.tsx
@@ -4,6 +4,12 @@ import SelectedSeats from "@/components/ui/order/SelectedSeats";
 const Order = () => {
     return (
         <div className='flex h-[100vh] w-[100vw] bg-black'>
+            <div className='container absolute left-0 right-0 z-10 w-full xl:top-[70px] 2xl:top-[170px]'>
+                <div className='inline-flex items-center text-white border-b border-solid font-medum ms-1 border-primary'>
+                    <div className='text-[20px]'>Spirited away</div>
+                    <div className='ml-[20px] text-[16px]'>1 h. 18 m.</div>
+                </div>
+            </div>
             <SelectedSeats />
             <SeatSelection />
         </div>
