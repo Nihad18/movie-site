e69477a896281d24a3bd9d89faef5cf4a9c5345c
MuxammedL <layicovmuxammed@gmail.com>
2024-07-22 12:20:04 +0400

    fix: fixed gitlab folder name

---
+++ src/components/order/SeatSelection.tsx
@@ -0,0 +1,105 @@
+import { useContext } from "react";
+import { DataContext } from "@/context/MainContext";
+import { SeatStatus } from "@/enums/SeatStatus";
+import { dates, times } from "@/lib/utils";
+
+const SeatSelection = () => {
+    const { activeDate, setActiveDate, activeTime, setActiveTime, seats, setSeats } = useContext(DataContext);
+
+    const handleSeatSelection = (rowIndex: number, seatIndex: number) => {
+        const newSeats = [...seats];
+        const selectedSeat = newSeats[rowIndex].seats[seatIndex];
+
+        if (selectedSeat.status == SeatStatus.AVAILABLE) {
+            selectedSeat.status = SeatStatus.SELECTED;
+        } else if (selectedSeat.status == SeatStatus.SELECTED) {
+            selectedSeat.status = SeatStatus.AVAILABLE;
+        }
+        setSeats([...newSeats]);
+    };
+    return (
+        <div className='h-full w-[100%] bg-black lg:w-[53%] lg:overflow-y-auto'>
+            <div>
+                <div className='pb-[14px] pt-[30px] text-center text-[18px] text-white md:pb-[34px] md:pt-[80px] md:text-[25px]'>Date</div>
+                <div className='flex justify-center'>
+                    {dates.map((date, index) => (
+                        <div
+                            key={index}
+                            onClick={() => setActiveDate(date)}
+                            className={`${
+                                date == activeDate ? "bg-gray-lightest-alt" : "bg-gray-darker-alt3"
+                            } mr-[13px] flex h-[58px] w-[40px] cursor-pointer items-center justify-center rounded-[30px] text-white transition-colors duration-200 last:mr-0 md:mr-[23px]`}
+                        >
+                            {date}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='mx-5 mb-[26px] mt-[32px] h-[1px] max-w-[447px] bg-gray-darkest-alt2 md:mx-auto md:mb-[46px] md:mt-[52px]'></div>
+
+            <div>
+                <div className='pb-[14px] text-center text-[18px] text-white md:pb-[34px] md:text-[25px]'>Time</div>
+                <div className='flex justify-center'>
+                    {times.map((time, index) => (
+                        <div
+                            key={index}
+                            onClick={() => setActiveTime(time)}
+                            className={`${
+                                time == activeTime ? "bg-gray-lightest-alt" : "bg-gray-darker-alt3"
+                            } mr-[13px] flex h-[32px] w-[60px] cursor-pointer items-center justify-center rounded-[30px] text-white transition-colors duration-200 last:mr-0 md:mr-[23px] md:h-[42px] md:w-[107px]`}
+                        >
+                            {time}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='mx-5 mb-[104px] mt-[32px] h-[1px] max-w-[638px] bg-gray-darkest-alt2 md:mx-auto md:mt-[52px]'></div>
+
+            <div className='relative'>
+                <div className='absolute left-[50%] top-[-25%] translate-x-[-50%]'>
+                    <img src='/LIGHT.svg' alt='light' />
+                </div>
+                {seats?.map((row, rowIndex) => (
+                    <div className='relative my-[5.5px] flex items-center justify-center' key={rowIndex}>
+                        <span className='pr-2 text-white'>{row.row}</span>
+                        <div className='flex w-[320px] justify-center md:w-[380px]'>
+                            {row.seats.map((seat: any, seatIndex: any) => (
+                                <div
+                                    key={seatIndex}
+                                    onClick={() => handleSeatSelection(rowIndex, seatIndex)}
+                                    className={`${
+                                        seat.status == SeatStatus.BOOKED
+                                            ? "cursor-not-allowed bg-gray-darker-alt4"
+                                            : seat.status == SeatStatus.AVAILABLE
+                                              ? "cursor-pointer bg-gray-light"
+                                              : "cursor-pointer bg-primary-dark"
+                                    } mx-[5px] flex h-[17px] w-[17px] flex-shrink-0 items-center justify-center rounded-full transition-colors duration-200 md:mx-[6px] md:h-[19px] md:w-[19px]`}
+                                ></div>
+                            ))}
+                        </div>
+                        <span className='pl-2 text-gray-light2'>{row.row}</span>
+                    </div>
+                ))}
+            </div>
+
+            <div className='mb-[80px] mt-[26px] flex justify-center text-[15px] text-white md:mb-[127px] md:mt-[36px]'>
+                <div className='mr-[25.26px] flex items-center'>
+                    <div className='mr-[12.63px] h-4 w-4 rounded-full bg-gray-light'></div>
+                    <div>Available</div>
+                </div>
+                <div className='mr-[25.26px] flex items-center'>
+                    <div className='mr-[12.63px] h-4 w-4 rounded-full bg-gray-darker-alt4'></div>
+                    <div>Booked</div>
+                </div>
+                <div className='flex items-center'>
+                    <div className='mr-[12.63px] h-4 w-4 rounded-full bg-primary-dark'></div>
+                    <div>Selected</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SeatSelection;
---
+++ src/pages/order/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import styles from "./order.module.scss"
+import { Button } from "@/components/ui/Button";
+import OrderModal from "@/components/orderModal";
+import SelectedSeats from "@/components/order/SelectedSeats";
+import SeatSelection from "@/components/order/SeatSelection";
+
+const Order = () => {
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const [congratsModal, setCongratsModal] = useState<boolean>(false)
+    return (
+        <div className='lg:flex h-[100vh] w-[100vw] bg-black'>
+            <div className='container absolute left-0 right-0 z-10 w-full top-[70px] 2xl:top-[100px]'>
+                <div className='inline-flex items-center text-white border-b border-solid font-medum ms-1 border-primary'>
+                    <div className='text-[20px]'>Spirited away</div>
+                    <div className='ml-[20px] text-[16px]'>1 h. 18 m.</div>
+                </div>
+            </div>
+            <SelectedSeats setShowModal={setShowModal} />
+            <SeatSelection />
+            {showModal &&
+                <OrderModal setShowModal={setShowModal} setCongratsModal={setCongratsModal} />
+            }
+            {
+                congratsModal &&
+                <div className={styles.modal}>
+                    <div className={styles["modal-inner"]}>
+                        <div className="relative h-full">
+                            <button
+                                className={styles.closeBtn}
+                                type="button"
+                                onClick={() => setCongratsModal(false)}
+                            >
+                                <svg
+                                    width="14"
+                                    height="14"
+                                    viewBox="0 0 14 14"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M7 7L13 13M1 13L7 7L1 13ZM13 1L6.99886 7L13 1ZM6.99886 7L1 1L6.99886 7Z"
+                                        stroke="#606060"
+                                        strokeWidth="1.5"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                </svg>
+                            </button>
+                            <div className="py-[54px] h-full md:px-[74px] text-center">
+                                <h5 className="font-semibold text-[20px] leading-[64px] mb-[14px] text-black">Congratulations!</h5>
+                                <p className="font-semibold text-[15px] leading-1.5 text-gray-darker">Your’ve bought two tickets. Please, save it on your <br />
+                                    device and show before the entering <br />
+                                    to the theatre</p>
+                                <Button className="max-w-[250px] w-full mt-7" type="button">Save tickets</Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+        </div>
+    );
+};
+
+export default Order;
