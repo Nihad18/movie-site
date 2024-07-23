b78522a73022b57453fba9ff28616775904597c8
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-23 11:18:44 +0400

    feat: added loading component

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+    return (
+        <div className='grid h-[80vh] place-items-center'>
+            <div className='w-10 h-10 text-black border-solid rounded-full border-[3px] dark:text-white border-t-primary animate-spin'></div>
+        </div>
+    );
+};
