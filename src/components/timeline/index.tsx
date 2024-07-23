3a02ac2a324108bb5b03a3ca2a3819a1ff48e74a
Nihad Balakishiev <nihadbalakisiyev18@gmail.com>
2024-07-23 10:21:33 +0400

    refactor(style): changed scss class naming

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -20,43 +20,47 @@ const Timeline = () => {
                     Pearls of Azerbaijani cinema
                 </div>
                 <div className='sub-container mx-auto pb-[28px] pt-[27px]'>
-                    <div className={`mb-[40px] flex h-[42px] items-center lg:mb-[81px] ${styles.lineContainer}`}>
+                    <div className={`mb-[40px] flex h-[42px] items-center lg:mb-[81px] ${styles["line-container"]}`}>
                         <div className={`bg-gray-lightest-alt5 dark:bg-gray-darkest-alt ${styles.line}`}></div>
                         <div
-                            className={`bg-black dark:bg-white ${styles.fillLine}`}
+                            className={`bg-black dark:bg-white ${styles["fill-line"]}`}
                             style={{ width: `${calculateLeftPercentage(selectedTime + 10)}%` }}
                         ></div>
                         {histories.map((time) => (
                             <div
                                 key={time}
-                                className={`${styles.markerContainer}`}
+                                className={`${styles["marker-container"]}`}
                                 style={{ left: `${calculateLeftPercentage(time + 10)}%` }}
                                 onClick={() => handleMarkerClick(time)}
                             >
                                 <div
-                                    className={`${time == selectedTime
-                                        ? `text-black dark:text-white ${styles.activeMarkerLabel}`
-                                        : `text-gray-lightest-alt5 dark:text-gray-normal-alt ${styles.markerLabel}`
-                                        }`}
+                                    className={`${
+                                        time == selectedTime
+                                            ? `text-black dark:text-white ${styles["active-marker-label"]}`
+                                            : `text-gray-lightest-alt5 dark:text-gray-normal-alt ${styles["marker-label"]}`
+                                    }`}
                                 >
                                     {time}
                                 </div>
                                 <div
-                                    className={`${time == selectedTime
-                                        ? `bg-black dark:bg-white ${styles.activeMarker}`
-                                        : `dark:bg-gray-normal-alt" bg-gray-lightest-alt5 ${styles.marker}`
-                                        }`}
+                                    className={`${
+                                        time == selectedTime
+                                            ? `bg-black dark:bg-white ${styles["active-marker"]}`
+                                            : `dark:bg-gray-normal-alt" bg-gray-lightest-alt5 ${styles.marker}`
+                                    }`}
                                 ></div>
                             </div>
                         ))}
                     </div>
                     <div className='flex max-w-[1084px] flex-col items-center justify-between md:flex-row'>
                         <div className={`${styles.image} mx-5 lg:mx-0`}>
-                            <LazyLoadImage alt={`image`} effect='opacity' src={MoviePoster} className='w-full h-full' />                
+                            <LazyLoadImage alt={`image`} effect='opacity' src={MoviePoster} className='w-full h-full' />
                         </div>
                         <div className='mx-5 mt-8 max-w-[415px] lg:mx-0 lg:mt-0'>
                             <div className='pb-[6px] text-[11px] font-light text-black dark:text-white'>OPERETTA</div>
-                            <div className='pb-[15px] lg:pb-[30px] text-[25px] font-medium text-gray-darker-alt2 dark:text-white'>Arshin Mal Alan</div>
+                            <div className='pb-[15px] text-[25px] font-medium text-gray-darker-alt2 dark:text-white lg:pb-[30px]'>
+                                Arshin Mal Alan
+                            </div>
                             <div className='text-[14px] font-medium text-gray-normal-alt2 dark:text-gray-lighter-alt5'>
                                 Arshin Mal Alan is a 4-act operetta composed by Azerbaijani composer Uzeyir Hajibeyov. The libretto of the work was
                                 also written by Uzeyir Hajibeyov in 1913 in St. Petersburg. The premiere of the opera was held on October 25, 1913 at
