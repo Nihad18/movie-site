c11359de43ab0ec507c440ecc07283ec56b26909
MuxammedL <layicovmuxammed@gmail.com>
2024-07-25 00:25:33 +0400

    feat: updated register schema for password securable

--- src/validations/ResgisterSchema.ts
+++ src/validations/ResgisterSchema.ts
@@ -3,15 +3,22 @@ import * as Yup from "yup";
 const namePattern = /^[A-Za-zƏəĞğİıÖöŞşÜüÇç\s-]+$/;
 
 export const RegisterSchema = Yup.object({
-    name: Yup.string()
+    firstName: Yup.string()
         .required("Name is required")
         .min(2, "The name must be at least 2 letters long")
         .matches(namePattern, "The name can only include letters, spaces, and hyphens"),
-    surname: Yup.string()
+    lastName: Yup.string()
         .required("Surname is required")
         .min(2, "The surname must be at least 2 letters long")
         .matches(namePattern, "The surname can only include letters, spaces, and hyphens"),
     email: Yup.string().required("Email is required").email("Email is not valid"),
-    password: Yup.string().required("Password is required").min(6, "At least 6 characters are required"),
-    dateOfBirth: Yup.string().required("Birth date is required"),
+    phone: Yup.string().required("Phone is required").length(16, "Cell phone must be exactly 7 digits long"),
+    password: Yup.string()
+        .required("Password is required")
+        .min(8, "At least 8 characters are required")
+        .matches(/[a-z]/, "At least one lowercase letter is required")
+        .matches(/[A-Z]/, "At least one uppercase letter is required")
+        .matches(/[0-9]/, "At least one number is required")
+        .matches(/[^a-zA-Z0-9]/, "At least one special character is required"),
+    birthDate: Yup.string().required("Birth date is required"),
 });
