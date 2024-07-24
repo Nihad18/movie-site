d9d05d52330dff262320dfd993357019356b8848
MuxammedL <layicovmuxammed@gmail.com>
2024-07-25 00:31:34 +0400

    feat: added an API to the loginAction function

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,8 @@
+import UserService from "@/services/UserService";
 import { AuthContextType, LoginData } from "@/types/AuthTypes";
 import { useContext, createContext, useState, ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
-
-
+import { jwtDecode } from "jwt-decode";
 type AuthProviderProps = {
     children: ReactNode;
 };
@@ -10,44 +10,32 @@ type AuthProviderProps = {
 const AuthContext = createContext<AuthContextType | null>(null);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
-    const [user, setUser] = useState<string>("");
+    const [user, setUser] = useState<Object>(() => {
+        const siteToken = localStorage.getItem("site");
+        return siteToken ? jwtDecode(siteToken) : {};
+    });
     const [token, setToken] = useState<string>(localStorage.getItem("site") || "");
     const navigate = useNavigate();
 
-    // const loginAction = async (data: LoginData) => {
-
-    //     try {
-    //         const response = await fetch("/api/auth/login", {
-    //             method: "POST",
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //             },
-    //             body: JSON.stringify(data),
-    //         });
-    //         const res = await response.json();
-    //         if (res.data) {
-    //             setUser(res.data.user);
-    //             setToken(res.token);
-    //             localStorage.setItem("site", res.token);
-    //             navigate("/");
-    //             return;
-    //         }
-    //         throw new Error(res.message);
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // };
     const loginAction = async (data: LoginData) => {
-        if (data) {
-            setUser(data.email);
-            setToken(data.password);
-            localStorage.setItem("site", data.email);
+        try {
+            const response = await UserService.login(data);
+            const accessToken = response.data.access_token;
+            setToken(accessToken);
+
+            const decodedToken = jwtDecode(accessToken);
+            console.log(decodedToken)
+            setUser(decodedToken);
+            localStorage.setItem("site", response.data.access_token);
             navigate("/");
             return;
+        } catch (error) {
+            console.error("Login failed:", error);
         }
-    }
+    };
+    
     const logOut = () => {
-        setUser("");
+        setUser({});
         setToken("");
         localStorage.removeItem("site");
         navigate("/login");
